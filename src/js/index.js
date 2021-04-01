3607b519c69cbd1d8863ee49e48073b3959c861e
Margaret <match14@yandex.ru>
2021-04-01 21:28:05 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
-});
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// $(window).scroll(function(){
+// 	if($(this).scrollTop()>300){
+// 		$('.js-move-up').addClass('visible');
+// 	}else{
+// 		$('.js-move-up').removeClass('visible');
+// 	}
+// });
+// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+/* select */
+if($('.js-select').length){
+	$('.js-select').select2({
+		minimumResultsForSearch: -1,
+	});
+}
