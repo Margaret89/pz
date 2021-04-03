df0266082fe0184cdfdace26a03763f2d980cdf5
Margaret <match14@yandex.ru>
2021-04-03 23:51:17 +0300

    main

--- src/js/index.js
+++ src/js/index.js
@@ -24,3 +24,64 @@ if($('.js-top-slider-img').length){
 		dots: true,
 	});
 }
+
+// show/hide faq
+if($('.js-faq').length){
+	$('.js-faq-head').on('click', function() {
+		var numElem = $(this).parent('.js-faq-item').data('num');
+
+		$('.js-faq-item').removeClass('active');
+		$(this).parent('.js-faq-item').addClass('active');
+		$(this).closest('.js-faq').find('.js-faq-item[data-num='+numElem+']').addClass('active');
+	});
+}
+
+// team slider
+if($('.js-team-slider').length){
+	$('.js-team-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: '<button id="prev" type="button" class="slider-prev"><svg class="icon ic-prev" width="24" height="65"><use xlink:href="assets/sprites/sprite.svg#ic-prev"></use></svg></button>',
+		nextArrow: '<button id="next" type="button" class="slider-next"><svg class="icon ic-next" width="24" height="65"><use xlink:href="assets/sprites/sprite.svg#ic-next"></use></svg></button>'
+	});
+}
+
+// quiz
+if($('.js-quiz-step').length){
+	var countSteps = $('.js-quiz-step').length;
+	var curStep = 1;
+	var error = false;
+
+	$('.js-quiz-next').on('click', function(e) {
+		curStep++;
+		
+		if($('.js-quiz-step.active .js-req-check').length){
+			if($('.js-req-check input:checkbox:checked').length == 0){
+				error = true;
+				$('.js-quiz-error').addClass('active');
+			}else{
+				error = false;
+				$('.js-quiz-error').removeClass('active');
+			}
+		}
+
+		if(error == false){
+			$('.js-quiz-step').removeClass('active');
+			$('.js-quiz-error').removeClass('active');
+
+			if(curStep > countSteps){
+				$('.js-quiz-success').addClass('active');
+				$(this).addClass('invisible');
+			}else{
+				e.preventDefault();
+				$('.js-quiz-step[data-step='+curStep+']').addClass('active');
+			}
+		}
+	});
+
+	$('.js-quiz').on('submit',function(e){
+		$(this)[0].reset();
+		e.preventDefault();
+	});
+}
